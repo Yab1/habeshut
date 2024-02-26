7462e0c555bd3670399e3ef776affe7d3497a199
Yab1 <yabilisanu@gmail.com>
2024-02-26 12:02:13 +0300

    Extended backgound colors and text color , added satoshi font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        light: {
+          primary: "#FFFFFF",
+          secondary: "#F7F8F8",
+        },
+        dark: {
+          primary: "#050F27",
+          secondary: "#111B31",
+        },
+      },
+      textColor: {
+        light: {
+          primary: "#050F27",
+        },
+        dark: {
+          primary: "#FFFFFF",
+        },
+      },
+    },
+    fontFamily: {
+      satoshi: ["Satoshi"],
+    },
   },
   plugins: [],
 };
