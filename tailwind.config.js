f97c94bafadd6ff0fa7029a6de1991c9f9e47743
Yab1 <yabilisanu@gmail.com>
2024-02-27 19:50:02 +0300

    Added primary border colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,14 @@ module.exports = {
           primary: "#050F27",
         },
       },
+      borderColor: {
+        light: {
+          primary: "#F7F8F8",
+        },
+        dark: {
+          primary: "#050F27",
+        },
+      },
     },
     fontFamily: {
       inter: ["Inter", "sans-serif"],
