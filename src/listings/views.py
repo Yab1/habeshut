1a3bcb8099622401d9da4d4acd47b52700ce3183
Yab1 <yabilisanu@gmail.com>
2024-02-27 19:50:27 +0300

    Defined a view for properties and properties detail

--- src/listings/views.py
+++ src/listings/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+class Properties_View(View):
+    def get(self, request):
+        return render(request, "listings/properties.html")
+
+
+class Property_Detail_View(View):
+    def get(self, request, id):
+        return render(request, "listings/property_detail.html", {"id": id})
