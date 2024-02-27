3c299266a284d42e4c041cf485419d843ffd2357
Yab1 <yabilisanu@gmail.com>
2024-02-27 19:49:18 +0300

    Defined a route for home, about and listings

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,27 +1,16 @@
-"""config URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
+from os import name
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", views.Home_View.as_view(), name="home"),
+    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    path(
+        "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
+    ),
     path("admin/", admin.site.urls),
+    path("properties/", include("listings.urls", namespace="listings")),
 ]
 
 if settings.DEBUG:
