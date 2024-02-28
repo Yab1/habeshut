2fbaf982527aa14627fd04ff0c67da71d09f87e8
Yab1 <yabilisanu@gmail.com>
2024-02-28 20:04:33 +0300

    Defined a view for agents and contacts page

--- src/realtors/views.py
+++ src/realtors/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+class Agents_View(View):
+    def get(self, request):
+        return render(request, "realtors/agents.html")
+
+
+class Contact_View(View):
+
+    def get(self, request):
+        return render(request, "realtors/contact.html")
