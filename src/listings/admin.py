31b8dc584da91c0e2804035a0c9fd717fc6958f9
Yab1 <yabilisanu@gmail.com>
2024-03-02 15:39:42 +0300

    Registed Property Type, Property Status, and Property

--- src/listings/admin.py
+++ src/listings/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Property, Status, Type
 
 # Register your models here.
+admin.site.register(Property)
+admin.site.register(Type)
+admin.site.register(Status)
