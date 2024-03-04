9127150ffac7d74afcb6fc21ee0d2c3703468cab
Yab1 <yabilisanu@gmail.com>
2024-03-04 20:04:37 +0300

    Add Agent model with unique fields and photo upload feature.

--- src/realtors/models.py
+++ src/realtors/models.py
@@ -1,3 +1,11 @@
+from enum import unique
 from django.db import models
 
+
 # Create your models here.
+class Agent(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    email = models.EmailField(max_length=64, unique=True)
+    phone = models.IntegerField(unique=True)
+    photo = models.ImageField(upload_to="agents/")
+    about = models.CharField(max_length=500)
