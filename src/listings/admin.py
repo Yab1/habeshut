f5e46e58dec5f42f053ebae92522041a062e6dda
Yab1 <yabilisanu@gmail.com>
2024-03-04 20:05:26 +0300

    Registered FloorPlan and Photos models

--- src/listings/admin.py
+++ src/listings/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Property, Status, Type
+from .models import FloorPlan, Photo, Property, Status, Type
 
 # Register your models here.
 admin.site.register(Property)
 admin.site.register(Type)
 admin.site.register(Status)
+admin.site.register(FloorPlan)
+admin.site.register(Photo)
